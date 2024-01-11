07a871b782f49c09335ae2fd874d7f86baab454b
Saiful264 <saifulislamx264@gmail.com>
2024-01-11 21:56:24 +0600

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">TODO!</h1>
+      <Button>hi</Button>
+    </>
+  );
+}
+
+export default App;
